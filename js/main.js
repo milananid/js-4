fc003eac9e3153e144b513e29b4783811957a3f3
milananid <33397026+milananid@users.noreply.github.com>
2018-03-15 14:33:09 +0100

    compiti

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 ;(function (){
   'use strict'
-  
+
   String.prototype.decodeHTML = function() {
     return $("<div>", {html: "" + this}).html();
   };
 
   var $main = $("#main"),
-  
+
   init = function() {
     // Do this when a page loads.
     //** INIT BUTTON **//
@@ -15,31 +15,31 @@
     	selector.doButton();
     }
   },
-  
+
   ajaxLoad = function(html) {
     document.title = 'Loaded via Ajax:' + html
       .match(/<title>(.*?)<\/title>/)[1]
       .trim()
-      .decodeHTML();	
-	
+      .decodeHTML();
+
 	$(window).trigger('ajaxpage.loaded');
     init();
     $(window).trigger('ajaxpage.ready');
   },
-  
+
   loadPage = function(href) {
   	$(window).trigger('ajaxpage.load');
-  	
+
   	var checkUrl = window.location.pathname.split( '/' );
   	checkUrl = checkUrl[checkUrl.length-1];
   	$('nav .current').removeClass('current');
     $('nav a[href="'+checkUrl+'"]').addClass('current');
-  	
+
     $main.load(href + " #main>*", ajaxLoad);
   };
-  
+
   init();
-  
+
   $(window).on("popstate", function(e) {
     if (e.originalEvent.state !== null) {
       loadPage(location.href);
@@ -57,7 +57,7 @@
       return false;
     }
   });
-  
+
   $(window).on({
   	'ajaxpage.loaded': function(){
   		console.log('evento: ajaxpage.loaded -  i contenuti sono stati caricati');
@@ -70,3 +70,27 @@
   		}
   })
 }());
+
+
+/***ESERCIZI
+modificare lo script affinché al click su un link
+con classe .ajax segua i seguenti task:
+1 - crei un nuovo blocco #main-N
+(dove N è un numero incrementale da 1 ad infinito)
+2 - carichi l'url richiamata e ne inietti l'html
+nel nuovo blocco inizializzandolo
+3 - faccia uscire il vecchio main con un'animazione
+da destra a sinistra
+4 - allo stesso modo faccia entrare il nuovo
+**/
+/**Approfondimenti:
+- SCROLL: cercare il metodo scrolltop()
+e vedere cosa succede se lo si applica in un evento scroll
+- IMAGE PRELOAD:
+https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
+var img = new Image();   // Create new img element
+img.addEventListener('load', function() {
+  // execute drawImage statements here
+}, false);
+img.src = 'myImage.png'; // Set source path
+**/
